76a67478fba678243445cf62f3bc86b816e1a6df
evan <guarneri1682@gmail.com>
2025-06-19 10:33:10 -0700

    Sign Up with OTP

--- components/auth/signup.tsx
+++ components/auth/signup.tsx
@@ -7,6 +7,7 @@ import { createBrowserSupabaseClient } from "utils/supabase/client";
 
 export default function SignUp({ setView }) {
   const [email, setEmail] = useState("");
+  const [otp, setOtp] = useState("");
   const [password, setPassword] = useState("");
   const [confirmationRequired, setConfirmationRequired] = useState(false);
 
@@ -28,35 +29,75 @@ export default function SignUp({ setView }) {
       }
     },
   });
+  const verifyOtpMutation = useMutation({
+    mutationFn: async () => {
+      const { data, error } = await supabase.auth.verifyOtp({
+        type: "signup",
+        email,
+        token: otp,
+        options: {},
+      });
+      if (data) {
+        setConfirmationRequired(true);
+      }
+      if (error) {
+        alert(error.message);
+      }
+    },
+  });
 
   return (
     <div className="flex flex-col gap-4">
       <div className="pt-10 pb-6 px-10 w-full flex flex-col items-center justify-center max-w-lg border border-gray-400 bg-white gap-2">
         <img src={"/images/inflearngram.png"} className="w-60 mb-6" />
-        <Input
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          type="email"
-          label="email"
-          className="w-full rounded-sm"
-        />
-        <Input
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          type="password"
-          label="password"
-          className="w-full rounded-sm"
-        />
+        {confirmationRequired ? (
+          <Input
+            value={otp}
+            onChange={(e) => setOtp(e.target.value)}
+            type="text"
+            label="otp"
+            className="w-full rounded-sm"
+            placeholder="Type 6 digit of OTP number "
+          />
+        ) : (
+          <>
+            <Input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              label="email"
+              className="w-full rounded-sm"
+            />
+            <Input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              label="password"
+              className="w-full rounded-sm"
+            />
+          </>
+        )}
+
         <Button
           onClick={() => {
-            signupMutation.mutate();
+            if (confirmationRequired) {
+              verifyOtpMutation.mutate();
+            } else {
+              signupMutation.mutate();
+            }
           }}
-          loading={signupMutation.isPending}
-          disabled={confirmationRequired}
+          loading={
+            confirmationRequired
+              ? verifyOtpMutation.isPending
+              : signupMutation.isPending
+          }
+          disabled={confirmationRequired
+              ? verifyOtpMutation.isPending
+              : signupMutation.isPending}
           color="light-blue"
           className="w-full text-md py-1"
         >
-          {confirmationRequired ? "check your email" : "Sign Up"}
+          {confirmationRequired ? "Verify" : "Sign Up"}
         </Button>
       </div>
 
